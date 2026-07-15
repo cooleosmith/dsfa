0df3ceab5d09e0a89b318bbae38df08bfc505098
smithlc00 <lstbw@umkc.edu>
2026-07-15 13:38:48 -0500

    Connect homepage to live league data
    Homepage now pulls league information from Google Sheets through Apps Script API.
    Created dynamic league pyramid display.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
-fetch("data/leagues.json")
-  .then(response => response.json())
-  .then(data => {
-    console.log("DSFA Data Loaded");
+const API_URL = "https://script.google.com/macros/s/AKfycbzoksSBIBVE5rY8x_tpjfC7L46L4xjPuJVsBvo3l3IGwFhCkQ0hg4fwvIjst1vnPjU/exec";
+
+
+fetch(API_URL)
+.then(response => response.json())
+.then(data => {
+
+    console.log("DSFA Live Data Loaded");
     console.log(data);
-  })
-  .catch(error => {
-    console.error("Error loading league data:", error);
-  });
+
+
+    const pyramid =
+        document.getElementById("league-pyramid");
+
+
+    if (pyramid) {
+
+        pyramid.innerHTML = "";
+
+
+        data.forEach(league => {
+
+            pyramid.innerHTML += `
+
+            <p>
+
+            ${league.icon}
+            <strong>${league.league_name}</strong>
+
+            <br>
+
+            ${league.teams} Teams
+
+            </p>
+
+            `;
+
+        });
+
+    }
+
+})
+.catch(error => {
+
+    console.error(
+        "Error loading DSFA data:",
+        error
+    );
+
+});
