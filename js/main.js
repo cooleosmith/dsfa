b92e9a25717f6abbd18934f7d09353c6786679ff
cooleosmith <cooleosmith@gmail.com>
2026-07-15 09:05:34 -0500

    Load league data from JSON file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("DSFA systems online 🚀");
+fetch("data/leagues.json")
+  .then(response => response.json())
+  .then(data => {
+    console.log("DSFA Data Loaded");
+    console.log(data);
+  })
+  .catch(error => {
+    console.error("Error loading league data:", error);
+  });
